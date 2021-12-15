ba30ff825faea584da3bbfa957f1249f3e7490c7
Marcelino Pangilinan <marbysneaker@gmail.com>
2021-12-15 15:10:12 -0800

    starting quesiton 1

--- Final/final1.asm
+++ Final/final1.asm
@@ -1,2 +1,33 @@
 # Final Exam : Quesiton No1
+
 # Name :Marcelino Pangilinan
+
+    .data
+
+msg1: .asciiz "\n Enter your number "
+
+    .data
+num3: .word 3
+
+    .text
+
+    .globl main 
+
+main:
+    lw  $t3, num3
+    lui $t0, 0xffff
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+loop:
