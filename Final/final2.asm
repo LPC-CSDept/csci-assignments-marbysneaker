7139d45bc6c880532026bf972baf95f939b69cce
Marcelino Pangilinan <marbysneaker@gmail.com>
2021-12-16 22:47:54 -0800

    1

--- Final/final2.asm
+++ Final/final2.asm
@@ -28,4 +28,24 @@ here:
 		
 		li 	$v0, 10     #   exit,if     it ever comes here     
 		syscall     
+
+         # KERNEL text
 		
+		.ktext  0x80000180     # kernel code starts here     
+		sw     	$v0, s1     #   We need to use these registers     
+		sw     	$a0, s2     #   not using the stack because the interrupt      
+							# might be triggered by a memory reference      
+							# using a bad value of the stack pointer  
+
+		mfc0 	$k0, $13     	#   Cause register     
+		srl     $a0, $k0, 2     #   Extract   ExcCode     Field  
+		
+		andi    $a0, $a0, 0x1f  #   Get the exception code   
+		
+	  
+		bne     $a0, $zero,   kdone     # Exception Code 0 is I/O. Only processing I/O here     
+
+		lui     $v0, 0xFFFF    	#   $v0 =   0xFFFF0000     
+		lw     	$a0, 4($v0)   # 	get the input key   
+		li  	$v0, 1 		#   print it here.      
+		syscall   
